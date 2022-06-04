19815c17991f727f07fd1a62fa7a6de71dba9663
JavaDotUtil <106878627+JavaDotUtil@users.noreply.github.com>
2022-06-04 22:33:49 +0700

    69.1

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ async function Unknown(context) {
 async function javathing(context) {
   await context.sendText('เท่ห์มาก');
 }
+
+async function timetable(context) {
+  await context.sendImage({
+    originalContentUrl: 'https://cdn.discordapp.com/attachments/981449206902456330/982667735362314320/IMG_1419.jpg',
+    previewImageUrl: 'https://cdn.discordapp.com/attachments/981449206902456330/982667735362314320/IMG_1419.jpg',
+  });
+}
+
 module.exports = async function App(context) {
-  return router([text('hi', SayHi), text('java', javathing), text('*', Unknown)]);
+  return router([text('hi', SayHi), text('java', javathing),text('ตารางสอน', timetable), text('*', Unknown)]);
 };
